a09517e871a409e47314a9986e0cb05c973bf678
mgould1799 <35778917+mgould1799@users.noreply.github.com>
2018-10-25 18:26:29 -0400

    made suggested changes that dr.bowring requested
    put deserialization and serialization methods within the person class. the driver needs to be included. need to also try and figure out junit for testing

--- src/Person.java
+++ src/Person.java
@@ -1,20 +1,57 @@
-import java.io.Serializable;
-
-
+import java.io.*;
 
 
 public class Person implements Serializable {
 
-    String name="Bob";
-    int age=10;
+    private String name;
+    private String DOB;
+
+    public Person(String DOB, String name){
+        this.DOB=DOB;
+        this.name=name;
+    }
 
 
 
-    public void setAge(int age){
-        this.age=age;
+    public void setDOB(String DOB){
+        this.DOB=DOB;
     }
     public void setName(String name){
         this.name=name;
+    }
+
+    public static void serializaton(Person p){
+        try {
+            FileOutputStream output = new FileOutputStream("personfile.ser");
+            ObjectOutputStream out = new ObjectOutputStream(output);
+
+            out.writeObject(p);
+            out.flush();
+            System.out.println("The object has been serialized.");
+        }
+        catch(Exception e){
+            System.out.println("error");
+        }
+    }
+    public static void deserialization() {
+        try {
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("personfile.ser"));
+            Person personNew = (Person) in.readObject();
+            System.out.println("The object has been deserialized");
+            System.out.println("The person's name is "+personNew.name + ", and their DOB is " +personNew.DOB + ".");
+
+            in.close();
+        }
+        catch(Exception e){
+            System.out.println("error");
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        Person bloop= new Person("081098","Meagan");
+
+        serializaton(bloop);
+        deserialization();
     }
 
 }
